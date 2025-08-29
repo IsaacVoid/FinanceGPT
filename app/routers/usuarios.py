eb98c35b1e55dc6a8cb75352e1b1f3ef2ef0ca8e
IsaacVoid <isaac20.espinosa@gmail.com>
2025-08-29 17:09:53 -0600

    endpoints
    enpoints:
     - get
     - post
     - delete
    added

--- app/routers/usuarios.py
+++ app/routers/usuarios.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel # Define modelo de datos usando pydantic
+from typing import Optional
 
 
 router = APIRouter()
@@ -13,8 +14,8 @@ class Usuario(BaseModel):
     nacionalidad: str
 
 # Definiciòn de enpoint POST
-@router.post("/usuarios") # Define enpoitit que responde a solicitudes POST en la ruta /usuarios
-# usuario: Usuario -> indica que el reques body debe coincidir con la estrucutura del modelo usuario
+@router.post("/usuarios") # Define endpoint que responde a solicitudes POST en la ruta /usuarios
+# usuario: Usuario -> indica que el request body debe coincidir con la estrucutura del modelo usuario
 async def crear_usuario(usuario: Usuario):
     return{
         "mensaje":"Usuario creado exitosamente",
@@ -27,3 +28,27 @@ async def crear_usuario(usuario: Usuario):
             "nacionalidad": usuario.nacionalidad
         }
     }
+
+@router.get("/usuarios")
+async def listar_usuarios(nombre: Optional[str] = None): # Se recomienda que nombre sea opcional
+    if nombre:
+        return {
+            "mensaje": f"Usuario {nombre} encontrado",
+            "datos": {
+                "nombre": nombre,
+                "email": f"{nombre}@example.com",
+                "edad": 25,
+                "direccion": "Calle Principal",
+                "telefono": "1234567890",
+                "nacionalidad": "MX"
+            }
+        }
+    else:
+        return{
+            "mensaje": "No se encontraron usuarios"
+        }
+
+@router.delete("/usuarios")
+async def eliminar_usuarios(nombre:str):
+    if nombre:
+        return {"mensaje": f"Usuario {nombre} modificado exitosamente"}
